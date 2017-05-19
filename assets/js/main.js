5ad7621b3f6b668a8807404100e19f5ddb44809a
berenisser <berenisser@gmail.com>
2017-05-19 01:02:06 -0400

    All styles done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+var espacioFotos = document.getElementById("fotitos");
+
+pictures.forEach(function(el){
+	var fotos = document.createElement("img");
+	fotos.setAttribute("src",el);
+	fotos.classList.add("cada-panda");
+	espacioFotos.appendChild(fotos);
+})
